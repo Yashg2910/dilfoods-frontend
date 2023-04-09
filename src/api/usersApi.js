bde5b236b6c16bb26193f29f8d9bcbf59f324670
Yash Gupta <yash@cerah.co>
2023-04-09 07:59:35 +0530

    Place order after OTP verification

--- src/api/usersApi.js
+++ src/api/usersApi.js
@@ -3,5 +3,11 @@ import {apiRequest} from "./apiFetch";
 export const usersApi = {
   async login(email, password) {
     return apiRequest('POST', "/login", {email, password});
+  },
+  async sendOtp(phone) {
+    return apiRequest('POST', "/send-otp", {phone});
+  },
+  async verifyOtp(phone, otp) {
+    return apiRequest('POST', "/verify-otp", {phone, otp});
   }
 };
--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Navbar from "../Navbar/Navbar";
 import "./Cart.css";
 import CartItem from './CartItem';
@@ -8,12 +8,19 @@ import { useSelector } from 'react-redux';
 import Button from "../Button/Button"
 import { ordersApi } from '../../api/ordersApi';
 import { useNavigate } from 'react-router-dom';
+import { toast } from 'react-toastify';
+import {usersApi} from "../../api/usersApi";
+import { userSession } from '../../api/userSession';
+import { login } from '../../redux/userSlice';
 
 function Cart() {
   const dispatch = useDispatch();
   const cart = useSelector((state) => state.cart);
   const userState = useSelector((state) => state.user);
   const navigate = useNavigate();
+  const [phone, setPhone] = useState("");
+  const [otp, setOtp] = useState();
+  const [otpState, setOtpState] = useState("unsent");
 
   const totalPrice = cart.items.reduce(((acc, item) => acc + (item.price * item.quantity)), 0);
 
@@ -26,15 +33,40 @@ function Cart() {
   }
 
   async function placeOrder() {
-    const userId = userState.user._id;
+    const userId = userState.user?._id;
     try {
       const res = await ordersApi.createOrder(userId, cart.items, totalPrice);
       dispatch(clearCart());
       navigate("/orders");
     } catch (e) {
       console.log("Error placing order", e);
+      toast.error("Error placing order");
     }
   }
+
+  async function sendOTP() {
+    try {
+      await usersApi.sendOtp(phone);
+      toast.info("OTP sent successfully")
+      setOtpState("sent");
+    } catch (e) {
+      toast.info("Error sending OTP");
+    }
+  }
+
+  async function verifyOtp() {
+    try {
+      const response = await usersApi.verifyOtp(phone, Number(otp));
+      toast.info("OTP verified successfully");
+      setOtpState("verified");
+      dispatch(login(response));
+      userSession.setUserToken(response.token);
+      userSession.setUser(response.user);
+    } catch (e) {
+      toast.info("Error sending OTP");
+    }
+  }
+
   return (
     <div className='page'>
       <Navbar/>
@@ -49,7 +81,15 @@ function Cart() {
           </> :
           <h3>No Items added yet</h3>
         }
-        <Button text={"Place Order"} disabled={cart.items.length === 0} onClick={placeOrder}/>
+        {otpState !== "verified" && <input type="text" name="phone" placeholder="Phone number" value={userState.user?.phone || phone} onChange={(e) => setPhone(e.target.value)} disabled={otpState === "sent"}/>}
+        {otpState === "unsent" && <Button text="Send OTP" onClick={sendOTP}/>}
+        {otpState === "sent" && 
+          <>
+            <input type="text" name="otp" placeholder="Enter OTP" value={otp} onChange={(e) => setOtp(e.target.value)} />
+            <Button text="Verify OTP" onClick={verifyOtp}/>
+          </>
+        }
+        <Button text={"Place Order"} disabled={cart.items.length === 0 || otpState !== "verified"} onClick={placeOrder}/>
       </div>
     </div>
   )
